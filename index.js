9f7b77c44867d9aa368bdb83aeeabae2744d7393
wyattdronen <137117182+wyattdronen@users.noreply.github.com>
2023-08-16 10:21:13 -0500

    finishing functions

--- index.js
+++ index.js
@@ -96,13 +96,117 @@ function empAdd(){
     });
 }
 
+function empByman(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "What is the employee's first name?"
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "What is the employee's last name?"
+        },  
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: "What is the employee's NEW manager's id?"
+
+        },
+    ])
+    .then((response) => {
+        sqL.query(`UPDATE employees SET manager_id = ${response.manager_id} WHERE first_name = '${response.first_name}' AND last_name = '${response.last_name}'`, (err, res) => {
+            if(err) throw err;
+            console.log("Manager updated!");
+            viewList();
+        });
+    });
+}
+
+function viewEmpMan(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: "What is the manager's id?"
+        }
+    ])
+    .then((response) => {
+        sqL.query(`SELECT * FROM employees WHERE manager_id = ${response.manager_id}`, (err, res) => {
+            if(err) throw err;
+            console.table(res);
+            viewList();
+        });
+    });
+}
+function viewEmpDep(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: "What is the employee id?"
+        }
+    ])
+   .then((response) => {
+    
+    });
+}
+function deleteDep(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department_id',
+            message: "What is the id of the department you want to delete?"
+        }
+    ])
+    .then((response) => {
+        sqL.query(`DELETE FROM department WHERE department_id = ${response.id}`, (err, res) => {
+            if(err) throw err;
+            console.log("Department deleted!");
+            viewList();
+        });
+    });
+}
+function deleteRole(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'role_id',
+            message: "What is the id of the role you want to delete?"
+        }
+    ])
+    .then((response) => {
+        sqL.query(`DELETE FROM roles WHERE id = ${response.id}`, (err, res) => {
+            if(err) throw err;
+            console.log("Role deleted!");
+            viewList();
+        });
+    });
+}
+function deleteEmp(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the id of the employee you want to delete?"
+        }
+    ])
+    .then((response) => {
+        sql.query(`DELETE FROM employees WHERE id = ${response.id}`, (err, res) => {
+            if(err) throw err;
+            console.log("Employee deleted!");
+            viewList();
+        });
+    });
+}
 
 
 function viewList(){
-    inquirer.prompt({
-        type: 'list',
+   const questions = 
+        {type: 'list',
         name: 'view',
-        message: "What would you like to view?",
+        message: "What would you like to do?",
         choices: [
             'Department',
             'Roles',
@@ -117,28 +221,61 @@ function viewList(){
             'Delete Role',
             'Delete Employee',
         ]
-    })
-    .then((response) => {
+    }
+    inquirer.prompt(questions).then(response => {
         if(response.view === 'Department'){
             select('department');
+            viewList();
         }
         else if(response.view === 'Roles'){
             select('roles');
+            viewList();
         }
         else if(response.view === 'Employees'){
             select('employees');
+            viewList();
         }
         else if(response.view === 'Add Department') {
             addDep();
+            //viewList();
         }
          else if(response.view === 'Add Role') {
             roleAdd();
+            //viewList();
         }
          else if(response.view === 'Add Employee') {
             empAdd();
-         }
-        });
-    };
+            //viewList();
+        }
+         else if(response.view === 'Update Employees by Manager') {
+            empByman();
+            //viewList();
+        }
+        else if(response.view === 'View Employees by Manager') {
+            viewEmpMan();
+            //viewList();
+        }
+        else if(response.view === 'View Employees by Department') {
+            viewEmpDep();
+           
+            //viewList();
+        }
+        else if(response.view === 'Delete Department') {
+            deleteDep();
+    
+            //viewList();
+        }
+        else if(response.view === 'Delete Role') {
+            deleteRole();
+            //viewList();
+        }
+        else if(response.view === 'Delete Employee') {
+            deleteEmp();
+            //viewList();
+        }
+    });
+        };
+    
 
 viewList()
 
